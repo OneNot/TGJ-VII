97be86c88f7b044c0ef5e84c6bdcf64939a97eb8
OneNot <www.1n07@gmail.com>
2018-11-16 19:42:29 +0200

    emt

---
+++ TGJ-VII/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 input;
+    private float inputX, inputY;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        input = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime);
+
+	}
+}
