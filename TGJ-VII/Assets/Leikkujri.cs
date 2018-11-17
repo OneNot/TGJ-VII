158db01c1e1ef229b384047617c22394624c79fa
Heinahattu <38815547+Heinahattu64@users.noreply.github.com>
2018-11-17 23:26:23 +0200

    Sirkkeli¨

--- TGJ-VII/Assets/Leikkujri.cs
+++ TGJ-VII/Assets/Leikkujri.cs
@@ -7,21 +7,34 @@ public class Leikkujri : MonoBehaviour {
     public float speed;
     public float spinningSpeed;
 
-
-	// Use this for initialization
 	void Start () {
-        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.parent.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.parent.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z); Turha mutta pidä varmuuden vuoksi mukana jos tarvitsee johonkin
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.right * spinningSpeed * speed * Time.deltaTime, Space.Self);
-        transform.Translate(transform.parent.transform.forward * speed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.right * spinningSpeed * speed * Time.deltaTime, Space.Self); //Terän pyörittäminen
+        transform.Translate(transform.parent.transform.forward * speed * Time.deltaTime, Space.World); //terän liikuttaminien
     }
 
+    //terän menosuunnan vaihto
     private void OnTriggerEnter (Collider other)
     {
         if (other.gameObject.CompareTag("SpinnerBox"))
             speed *= -1;
+
+        if(other.gameObject.CompareTag("TubeDude"))
+        {
+            TubeDudeBehavior brainToKill = other.gameObject.GetComponent<TubeDudeBehavior>();
+            //brainToKill.ActivateRagdoll();
+            brainToKill.gameObject.tag = "DeadDude";
+            brainToKill.enabled = false; //lobotomize
+        }
+        else if (other.gameObject.CompareTag("ControlledDude"))
+        {
+            other.gameObject.tag = "DeadDude";
+            GameObject.FindGameObjectWithTag("SpawnController").GetComponent<ControlRespawn>().ControlSwap();
+            other.gameObject.GetComponent<PlayerController>().enabled = false;
+        }
+
     }
 }
