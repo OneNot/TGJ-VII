65319863cc5825253c976b0b91eea82ad7f65487
OneNot <www.1n07@gmail.com>
2018-11-17 20:46:48 +0200

    KF

---
+++ TGJ-VII/Assets/Scripts/GoalKiller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalKiller : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("TubeDude"))
+        {
+            transform.parent.gameObject.GetComponent<TuubiRotator>().DudesInRange.Remove(other.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ TGJ-VII/Assets/Scripts/Sucker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sucker : MonoBehaviour {
+
+    private Vector3 eyeOfTheStorm;
+    public float Succ = 10;
+
+	// Use this for initialization
+	void Start () {
+        eyeOfTheStorm = transform.GetChild(0).transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("TubeDude"))
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce((eyeOfTheStorm - other.gameObject.transform.position).normalized * Time.deltaTime * Succ);
+        }
+    }
+}
