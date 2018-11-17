58a34d0bc5abad1ee1dd265758d3458d7299c132
hegeteus <45102227+hegeteus@users.noreply.github.com>
2018-11-17 17:09:19 +0200

    Hegenstuff

--- TGJ-VII/Assets/Menubutton.cs
+++ TGJ-VII/Assets/Menubutton.cs
@@ -9,7 +9,14 @@ public class Menubutton : MonoBehaviour {
 
     public GameObject nextSelectable;
     public GameObject previousSelectable;
+    public GameObject UIController;
 
+    public GameObject rightSelectable;
+    public GameObject leftSelectable;
+     
+    //Nämä ovat sitä varten, että nappulat vaihtuvat esim. settings valikossa pelin tilasta riippuen
+    public string disableButton;
+    public string enableButton;
 
     // Etsitään tesktiobjekti ja ylikirjoitetaan se esim: --> Back --> Resume
     public string overrideTextObject;
@@ -20,20 +27,33 @@ public class Menubutton : MonoBehaviour {
     // Painike joka uudessa valikossa on aktiivinen
     public string defaultActiveButton;
 
+    //Tämä on nappuloiden paluutoimintoa varten. Ei pelkästään esc, kaikki jotka käyttävät "Cancel"
+    public bool listensToEsc;
+
+    public string SceneName;
+
+
     
 
 
 	// Use this for initialization
 	void Start () {
-       
+        UIController = GameObject.Find("UIController");
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        
+        if(listensToEsc == true && UIController.GetComponent<UIController>().gameActive == false && gameObject.GetComponentInParent<Canvas>().enabled == true) 
+        {
+            if (Input.GetButtonDown("Cancel"))
+            {
+                UIController.GetComponent<UIController>().enable = false;
+                gameObject.GetComponent<Selectable>().Select();
+                gameObject.GetComponent<Button>().onClick.Invoke();
+            }
 
-
+        }
         
     }
 
@@ -57,7 +77,11 @@ public class Menubutton : MonoBehaviour {
 
         if (GameObject.Find(openPanel) != null)
         {
+           
+
             GameObject.Find(openPanel).GetComponent<Canvas>().enabled = true;
+
+            if(gameObject.GetComponentInParent<Canvas>()!=null)
             gameObject.GetComponentInParent<Canvas>().enabled = false;
         }
 
@@ -71,6 +95,8 @@ public class Menubutton : MonoBehaviour {
     //Palautuu päävalikkoon tai takaisin peliin, riippuen onko päävalikossa
     public void BackToMainOrGame()
     {
+        
+
         if(SceneManager.GetActiveScene().name == "Mainmenu")
         {
             GameObject.Find("Mainmenu").GetComponent<Canvas>().enabled = true;
@@ -79,17 +105,51 @@ public class Menubutton : MonoBehaviour {
             GameObject.Find("UIController").GetComponent<UIController>().targetSelectable = GameObject.Find(defaultActiveButton);
         }
 
-        /*else
-        { 
-            gameObject.GetComponentInParent<Canvas>().enabled = false;
-            //Resume toiminto
-        }
-        */
+        else if(UIController.GetComponent<UIController>().gameActive == false)
+            ResumeGame();
+      
+        
     }
 
     //Palautuu päävalikkoon tai lopettaa pelin jos ollaan siellä
-    public void BackToMainOrQuit()
-    {
 
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        gameObject.GetComponentInParent<Canvas>().enabled = false;
+        UIController.GetComponent<UIController>().gameActive = true;
+        UIController.GetComponent<UIController>().lastState = "Resumed";
+    }
+
+    public void EnableButton()
+    {
+        UIController.GetComponent<UIController>().ActivateButton(enableButton);
+    }
+
+    public void DisableButton()
+    {
+        UIController.GetComponent<UIController>().DisableButton(disableButton);
+    }
+
+    public void StartScene()
+    {
+        UIController.GetComponent<UIController>().gameActive = true;
+        SceneManager.LoadScene(SceneName);
+        gameObject.GetComponentInParent<Canvas>().enabled = false;
+    }
+
+    public void QuitToMenu()
+    {
+        Destroy(GameObject.Find("Settings"));
+        Destroy(GameObject.Find("Mainmenu"));
+        Destroy(GameObject.Find("UIController"));
+        Destroy(GameObject.Find("EventSystem"));
+        SceneManager.LoadScene("Mainmenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
--- TGJ-VII/Assets/UIController.cs
+++ TGJ-VII/Assets/UIController.cs
@@ -7,22 +7,43 @@ using UnityEngine.EventSystems;
 
 public class UIController : MonoBehaviour
 {
+    //Tarkistaa onko peli päällä, eikä esim paused
+    public bool gameActive;
+    public string lastState = "Resumed";
+
+    public bool enable = true;
+
     bool verticalAxisAvailable;
     bool horizontalAxisAvailable;
 
     public GameObject targetSelectable;
 
-	// Use this for initialization
-	void Start () {
-        PlayerPrefs.DeleteAll();
+    private void Awake()
+    {
+        //Käytä tätä testaamaan, että oletusasetukset iskeytyvät oikein
+        //PlayerPrefs.DeleteAll();
+    }
 
+    // Use this for initialization
+    void Start () {
+    
         targetSelectable = GameObject.Find("Mainmenu_defaultselectable");
         AudioListener.volume = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void LateUpdate ()
     {
+        if(targetSelectable != null)
+        print(targetSelectable.name);
+
+        if (Input.GetButtonDown("Cancel") && gameActive == true && lastState == "Resumed" && enable == true)
+        {
+                PauseGame();
+        }
+
+        enable = true;
+
         if (Input.GetAxis("Vertical") == 0)
             verticalAxisAvailable = true;
 
@@ -35,7 +56,7 @@ public class UIController : MonoBehaviour
         {
             verticalAxisAvailable = false;
 
-            if (targetSelectable.GetComponent<Menubutton>().nextSelectable != null)
+            if (targetSelectable.GetComponent<Menubutton>().nextSelectable != null && targetSelectable.GetComponent<Menubutton>().nextSelectable.GetComponent<Selectable>().enabled == true)
             {
                 targetSelectable.GetComponent<Menubutton>().nextSelectable.GetComponentInChildren<Selectable>().Select();
                 targetSelectable = targetSelectable.GetComponent<Menubutton>().nextSelectable;
@@ -47,7 +68,7 @@ public class UIController : MonoBehaviour
         {
             verticalAxisAvailable = false;
 
-            if (targetSelectable.GetComponent<Menubutton>().previousSelectable != null)
+            if (targetSelectable.GetComponent<Menubutton>().previousSelectable != null && targetSelectable.GetComponent<Menubutton>().previousSelectable.GetComponent<Selectable>().enabled == true)
             {
                 targetSelectable.GetComponent<Menubutton>().previousSelectable.GetComponentInChildren<Selectable>().Select();
                 targetSelectable = targetSelectable.GetComponent<Menubutton>().previousSelectable;
@@ -55,29 +76,44 @@ public class UIController : MonoBehaviour
 
         }
 
-        if (Input.GetAxisRaw("Horizontal") == -1 && verticalAxisAvailable == true)
+        if (Input.GetAxisRaw("Horizontal") == -1)
         {
-            verticalAxisAvailable = false;
+           
 
             if (targetSelectable.GetComponentInParent<Slider>() != null)
             {
                 targetSelectable.GetComponentInParent<Slider>().value--;
             }
 
+            else if (targetSelectable.GetComponent<Menubutton>().leftSelectable != null && horizontalAxisAvailable == true && targetSelectable.GetComponent<Menubutton>().leftSelectable.GetComponent<Selectable>().enabled == true)
+            {
+                targetSelectable.GetComponent<Menubutton>().leftSelectable.GetComponentInChildren<Selectable>().Select();
+                targetSelectable = targetSelectable.GetComponent<Menubutton>().leftSelectable;
+            }
+
+            horizontalAxisAvailable = false;
+
         }
 
-        if (Input.GetAxisRaw("Horizontal") == 1 && verticalAxisAvailable == true)
+        if (Input.GetAxisRaw("Horizontal") == 1)
         {
-            verticalAxisAvailable = false;
+            
 
             if (targetSelectable.GetComponentInParent<Slider>() != null)
             {
                 targetSelectable.GetComponentInParent<Slider>().value++;
             }
 
+            else if (targetSelectable.GetComponent<Menubutton>().rightSelectable != null && horizontalAxisAvailable == true && targetSelectable.GetComponent<Menubutton>().rightSelectable.GetComponent<Selectable>().enabled == true)
+            {
+                targetSelectable.GetComponent<Menubutton>().rightSelectable.GetComponentInChildren<Selectable>().Select();
+                targetSelectable = targetSelectable.GetComponent<Menubutton>().rightSelectable;
+            }
+
+            horizontalAxisAvailable = false;
         }
 
-        if (Input.GetButton("Submit"))
+        if (Input.GetButton("Submit") && targetSelectable != null)
         {
             if(targetSelectable.GetComponent<Button>() != null)
             targetSelectable.GetComponent<Button>().onClick.Invoke();
@@ -85,6 +121,30 @@ public class UIController : MonoBehaviour
 
         
 
+    }
+
+    public void PauseGame()
+    {
+        lastState = "Paused";
+        gameActive = false;
+        Time.timeScale = 0.0001f;
+        gameObject.GetComponent<Menubutton>().OpenPanel();
+        gameObject.GetComponent<Menubutton>().EnableButton();
+        targetSelectable.GetComponent<Selectable>().Select();
+    }
+
+    public void ActivateButton(string buttonName)
+    {
+        GameObject.Find(buttonName).GetComponent<Selectable>().enabled = true;
+        GameObject.Find(buttonName).GetComponent<Image>().enabled = true;
+        GameObject.Find(buttonName).GetComponentInChildren<Text>().enabled = true;
+    }
+
+    public void DisableButton(string buttonName)
+    {
+        GameObject.Find(buttonName).GetComponent<Selectable>().enabled = false;
+        GameObject.Find(buttonName).GetComponent<Image>().enabled = false;
+        GameObject.Find(buttonName).GetComponentInChildren<Text>().enabled = false;
     }
 
 }
