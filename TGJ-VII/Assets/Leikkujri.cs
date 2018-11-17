2a99cf2f5622538af89682a4e12af95f8f643099
Heinahattu <38815547+Heinahattu64@users.noreply.github.com>
2018-11-17 23:18:08 +0200

    Leikkuri

--- TGJ-VII/Assets/Leikkujri.cs
+++ TGJ-VII/Assets/Leikkujri.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Leikkujri : MonoBehaviour {
 
+    public float speed;
+    public float spinningSpeed;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.parent.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.right * 50 * Time.deltaTime, Space.Self);
+        transform.Rotate(Vector3.right * spinningSpeed * speed * Time.deltaTime, Space.Self);
+        transform.Translate(transform.parent.transform.forward * speed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("SpinnerBox"))
+            speed *= -1;
     }
 }
