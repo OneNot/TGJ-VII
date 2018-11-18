a7b95d31cb2103d79b186e9d182cca1a32d17702
OneNot <www.1n07@gmail.com>
2018-11-18 06:14:04 +0200

    audioooo

--- TGJ-VII/Assets/Scripts/BloodyMessScript.cs
+++ TGJ-VII/Assets/Scripts/BloodyMessScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class BloodyMessScript : MonoBehaviour {
 
     private float startTime;
+    private AudioSource aS;
 
 	// Use this for initialization
 	void Start () {
         startTime = Time.time;
+        aS = GetComponent<AudioSource>();
+        aS.volume = PlayerPrefs.GetFloat("SFXVolume", 1f);
+        aS.Play();
 	}
 	
 	// Update is called once per frame
--- TGJ-VII/Assets/Scripts/Leikkujri.cs
+++ TGJ-VII/Assets/Scripts/Leikkujri.cs
@@ -8,14 +8,28 @@ public class Leikkujri : MonoBehaviour {
 
     public float speed;
     public float spinningSpeed;
+    public float OnHitPitchDuration = 0.2f;
+    public float HowMuchToPitch = 2.5f;
+
+    private float pitchUpTime;
+    private bool pitched;
+    private AudioSource aS;
 
 	void Start () {
-        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.parent.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z); Turha mutta pidä varmuuden vuoksi mukana jos tarvitsee johonkin
+        aS = GetComponent<AudioSource>();
+        aS.volume = PlayerPrefs.GetFloat("SFXVolume", 1f) / 5;
+        aS.Play();
 	}
 	
 	void Update () {
         transform.Rotate(Vector3.right * spinningSpeed * 100 * Time.deltaTime, Space.Self); //Terän pyörittäminen
         transform.Translate(transform.parent.transform.forward * speed * Time.deltaTime, Space.World); //terän liikuttaminien
+
+        if (pitched && Time.time - pitchUpTime > OnHitPitchDuration)
+        {
+            aS.pitch = 1f;
+            pitched = false;
+        }
     }
 
     //terän menosuunnan vaihto
@@ -30,9 +44,11 @@ public class Leikkujri : MonoBehaviour {
             //brainToKill.ActivateRagdoll();
             brainToKill.gameObject.tag = "DeadDude";
             brainToKill.StopEffect();
+            aS.pitch = HowMuchToPitch;
+            pitchUpTime = Time.time;
+            pitched = true;
             Instantiate(BloodyMessPrefabRef, other.transform.position, Quaternion.Euler(Vector3.zero)); //spawns a bloody explosion (handles removal itself afterwards)
             Destroy(other.gameObject);
-            //brainToKill.enabled = false; //lobotomize
         }
         else if (other.gameObject.CompareTag("ControlledDude"))
         {
@@ -41,7 +57,9 @@ public class Leikkujri : MonoBehaviour {
             other.gameObject.tag = "DeadDude";
             GameObject.FindGameObjectWithTag("SpawnController").GetComponent<ControlRespawn>().ControlSwap();
             brainToKill.StopEffect();
-            //brainToKill.enabled = false;
+            aS.pitch = 1.8f;
+            pitchUpTime = Time.time;
+            pitched = true;
             Instantiate(BloodyMessPrefabRef, other.transform.position, Quaternion.Euler(Vector3.zero)); //spawns a bloody explosion (handles removal itself afterwards)
             Destroy(other.gameObject);
         }
